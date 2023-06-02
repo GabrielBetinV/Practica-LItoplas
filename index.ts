676dce3615092cd43ce2330b600b4675a882a8ff
GabrielBetinV <102318057+GabrielBetinV@users.noreply.github.com>
2023-06-02 08:56:34 -0500

    2 files modified

--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+//const appDiv: HTMLElement = document.getElementById('app');
+//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 var dato = 10;
 let dato2 = 11;
 const dato3 = 3;
+
+// Inferencia de datos
 
 // Constante -> No se puede reasignar-cambiar
 const a = 10;
@@ -26,3 +28,29 @@ let d: number = 10;
 // Tipo ANY, si quiero saber el tipo de este dato nos tocara
 // ir hasta la definicion para poder saber
 const e: any = 10;
+
+// Booleanos
+let isSuperman: boolean = true;
+let isBatman: boolean = false;
+
+// Forma para condiciones
+isSuperman = true && false;
+
+console.log({ isSuperman });
+
+// Condicion con operador ternario
+isSuperman = 1 + 1 == 2 ? true : false;
+
+console.log({ isSuperman });
+
+// Condicionales normales
+
+if (isSuperman) {
+  true;
+} else {
+  false;
+}
+console.log({ isSuperman });
+
+// No es buena practica anidar en el operador ternario, asi
+isSuperman = 1 + 1 == 2 ? true : 10 < 10 ? true : false;
